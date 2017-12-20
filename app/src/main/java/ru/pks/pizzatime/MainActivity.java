47a0dcefc6fff90fe9f687d295dedfe8cf1d4aab
kirill <kirill_nn@inbox.ru>
2017-12-20 23:23:41 +0300

    Layout is finish

--- app/src/main/java/ru/pks/pizzatime/MainActivity.java
+++ app/src/main/java/ru/pks/pizzatime/MainActivity.java
@@ -2,9 +2,14 @@ package ru.pks.pizzatime;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private int item, itemBonus, itemTotal;
+    TextView tagline, productName, itemView, itemBonusView, itemTotalView;
+    ImageButton plus, minus, offer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

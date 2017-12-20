adc424eb0c9d8bab2ec829061770c364032c7b20
kirill <kirill_nn@inbox.ru>
2017-12-21 00:12:52 +0300

    Version 0.1 finished

--- app/src/main/java/ru/pks/pizzatime/MainActivity.java
+++ app/src/main/java/ru/pks/pizzatime/MainActivity.java
@@ -2,12 +2,14 @@ package ru.pks.pizzatime;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private int item, itemBonus, itemTotal;
-    TextView tagline, productName, itemView, itemBonusView, itemTotalView;
+    TextView itemView, itemBonusView, itemTotalView;
     ImageButton plus, minus, offer;
 
 
@@ -15,5 +17,71 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        item = 0;
+        itemBonus = 0;
+        itemTotal = 0;
+
+        itemView = findViewById(R.id.itemView);
+        itemBonusView = findViewById(R.id.itemBonusView);
+        itemTotalView = findViewById(R.id.itemTotalView);
+
+        offer = findViewById(R.id.offer);
+        offer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.offer_3_1, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        plus = findViewById(R.id.plus);
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemPlus();
+                updateUI();
+            }
+        });
+
+        minus = findViewById(R.id.minus);
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemMinus();
+                updateUI();
+            }
+        });
+
+        updateUI();
+    }
+
+    void itemPlus() {
+        item++;
+        itemBonus();
+        itemTotal = item + itemBonus;
+
+    }
+
+    void itemMinus() {
+        if (item > 0) {
+            item--;
+            itemBonus();
+            itemTotal = item + itemBonus;
+        } else {
+            item = 0;
+            itemBonus = 0;
+            itemTotal = 0;
+        }
+
+    }
+
+    void itemBonus() {
+        itemBonus = (int) Math.ceil(item / 3);
+    }
+
+    void updateUI() {
+        itemView.setText(getString(R.string.you) + item);
+        itemBonusView.setText(getString(R.string.bonus) + itemBonus);
+        itemTotalView.setText(getString(R.string.total) + itemTotal);
     }
 }

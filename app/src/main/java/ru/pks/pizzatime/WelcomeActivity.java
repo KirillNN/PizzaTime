0e54fc199a7d09b5386723e045006fdf94ff295a
kirill <kirill_nn@inbox.ru>
2017-12-22 21:15:06 +0300

    startActivity

--- app/src/main/java/ru/pks/pizzatime/WelcomeActivity.java
+++ app/src/main/java/ru/pks/pizzatime/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package ru.pks.pizzatime;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,23 +20,22 @@ public class WelcomeActivity extends AppCompatActivity {
 
         final String[] itemNames = {
                 getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
-                getString(R.string.raphael_pizza),
                 getString(R.string.splinter_burger)
         };
 
         itemView = findViewById(R.id.ListView);
         ArrayAdapter<String> itemAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemNames);
         itemView.setAdapter(itemAdapter);
+        itemView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(WelcomeActivity.this, RaphaelActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
 }

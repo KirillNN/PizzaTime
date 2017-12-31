4796fb4e9a2c2a055ed4229b914c3748ce97fe72
kirill <kirill_nn@inbox.ru>
2017-12-31 20:16:48 +0300

    Material design. Fist steps on my way :-)

--- app/src/main/java/ru/pks/pizzatime/WelcomeActivity.java
+++ app/src/main/java/ru/pks/pizzatime/WelcomeActivity.java
@@ -3,26 +3,26 @@ package ru.pks.pizzatime;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class WelcomeActivity extends MainActivity {
 
-    private static final int REQUEST = 1;
+    private static final int REQUEST_RAPHAEL = 1;
     private static final String TAG = "WelcomeActivity";
     private static final String ORDER_FULL = "order_full";
 
     private String order;
     private String orderFull;
 
-    ListView itemView;
+
     TextView orderView;
-    Button sendOrder;
+    FloatingActionButton sendOrder;
+    CardView raphael;
+    CardView april;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,12 +85,12 @@ public class WelcomeActivity extends MainActivity {
 
     protected void raphaelStarted() {
         Intent intent = new Intent(WelcomeActivity.this, RaphaelActivity.class);
-        startActivityForResult(intent, REQUEST);
+        startActivityForResult(intent, REQUEST_RAPHAEL);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+        if (requestCode == REQUEST_RAPHAEL && resultCode == Activity.RESULT_OK && data != null) {
             orderFull = data.getStringExtra(RaphaelActivity.ORDER_RAFAEL);
             updateUI();
         }
@@ -99,24 +99,11 @@ public class WelcomeActivity extends MainActivity {
     private void initUI() {
         order = getString(R.string.order_text_view);
 
-        final String[] itemNames = {
-                getString(R.string.raphael_pizza),
-                getString(R.string.splinter_burger)
-        };
-
-        itemView = findViewById(R.id.ListView);
-        ArrayAdapter<String> itemAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemNames);
-        itemView.setAdapter(itemAdapter);
-        itemView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        raphael = findViewById(R.id.raphael);
+        raphael.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        raphaelStarted();
-                        break;
-                    default:
-                        inDevelop();
-                }
+            public void onClick(View v) {
+                raphaelStarted();
             }
         });
 

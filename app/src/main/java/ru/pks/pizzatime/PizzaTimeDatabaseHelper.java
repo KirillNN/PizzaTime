b51dd2ac2194d93d9ccb71b0fb9d5c67cbcfdf08
Kirill <kirill_nn@inbox.ru>
2018-01-05 12:14:31 +0300

    SQLite first step. Part 2

--- app/src/main/java/ru/pks/pizzatime/PizzaTimeDatabaseHelper.java
+++ app/src/main/java/ru/pks/pizzatime/PizzaTimeDatabaseHelper.java
@@ -1,5 +1,6 @@
 package ru.pks.pizzatime;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,11 +16,28 @@ public class PizzaTimeDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        updateMyDatabase(db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
 
+    private static void insertPizza(SQLiteDatabase db, String name, int orderQuantity) {
+        ContentValues pizzaValues = new ContentValues();
+        pizzaValues.put("NAME", name);
+        pizzaValues.put("ORDER_QUANTITY", orderQuantity);
+        db.insert("PIZZA", null, pizzaValues);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE PIZZA ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "ORDER_QUANTITY INTEGER);");
+            insertPizza(db, "RaphaelPIZZA", 0);
+        }
     }
 }
